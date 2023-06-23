c8e561f9f1f7dd9020247b24e0b98320a2a84e6e
Alfina Damayanti <alfiinady@gmail.com>
2023-06-23 13:07:26 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Alfina Damayanti" // TODO: replace this
+const studentId = "FE5163274" // TODO: replace this
+
+export { url, studentName, studentId }
